1bd414e42fbba044e1fea53129375ecd6a7aea5a
Erizium <tobbe.osterlin@gmail.com>
2021-01-13 13:56:54 +0100

    Checks gameState and currentPlayer. ongoingGame checks if fields are left or not. This will stop any moves from being played when all fields are filled. Also, this will "occupy" the field you pressed on with the current players value.

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     var currentPlayer = 1
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    var ongoingGame = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,20 +19,24 @@ class ViewController: UIViewController {
     }
 
     @IBAction func playButtons(_ sender: Any) {
-        
+        //Will keep the game running until all fields are gone.
+        if gameState[(sender as AnyObject).tag] == 0 && ongoingGame == true{
+            //This places currentPlayer value in gameState
+            gameState[(sender as AnyObject).tag] = currentPlayer
     
-        if currentPlayer == 1 {
-            (sender as AnyObject).setImage(UIImage(
+            if currentPlayer == 1 {
+                (sender as AnyObject).setImage(UIImage(
                                 named: "cross.png"),
                             for: UIControl.State())
-            currentPlayer = 2
+                currentPlayer = 2
             
-        } else {
-            (sender as AnyObject).setImage(UIImage(
+            } else {
+                (sender as AnyObject).setImage(UIImage(
                                 named: "nought.jpeg"),
                             for: UIControl.State())
-            currentPlayer = 1
+                currentPlayer = 1
+            }
         }
-        }
+    }
 
 }
